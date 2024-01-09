0c2d577a69a8431328d79fc130199693c473d81f
jeankalyson <jeankalyson16@gmail.com>
2024-01-08 23:09:18 -0300

    making migrations and create the templates

--- teste_app/models.py
+++ teste_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length = 200, default="texto")
+    date_added = models.DateTimeField(auto_now_add=True)
+    # método para mostrar o nome do tópico criado aqui 
+    def __str__(self):
+        return self.text
+    
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete= models.CASCADE)
+    text = models.TextField( default="texto")
+    date_added = models.DateTimeField(auto_now_add=True,null=True)
+     
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __srt__(self):
+        return self.text
